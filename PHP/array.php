b4bc1c5e8e26df3458bc5dfa3761352788f36e68
melisa-2 <melisaterstenaa@gmail.com>
2024-11-01 16:47:02 +0100

    Add files via upload

--- PHP/array.php
+++ PHP/array.php
@@ -1,47 +1,51 @@
-<?php 
-$sports = array ( 'Football','Basketball','Handball','Volleyball' );
+<?php
 
+// $dogs=array(
+//     array('chiuaua','mexico',20),
+//     array('husky','siberia',15),
+//     array('bulldog','england',10)
+// );
 
-$sport = ['Football','Basketball','Handball','Volleyball'];
+// echo $dogs[0][0] . ": origin: " . $dogs[0][1] . ", life span: " . $dogs[0][2];
 
-// echo $sport[2];
-// echo end($sport);
-// echo count($sport);
-
-// for($i=0;$i<4;$i++){
-//     echo $sport[$i]. "<br>";
+// for($row=0;$row<3;$row++){
+//     echo "<p> <b> Row number $row </b> </p>";
+//     echo "<ul>";
+//     for($col=0;$col<3;$col++){
+//         echo"<li>" . $dogs[$row][$col] . "</li>";
+//     }
+//     echo"</ul>";
 // }
 
-// $leng=count($sport);
-// for ($i=0;$1<$leng;$i++){
-//     echo $sport[$i] "<br>";
+// $arrays=array(
+//     array(1,2,3),
+//     array(1,2,3),
+//     array(1,2,3)
+// );
+
+// for($i=1;$i<4;$i++){
+//     for($j=1;$j<4;$j++){
+//         echo "array: $i element $j <br>";
+//     }
 // }
 
-//array_push($array,$value) // e shton elementin $value ne fund te array
-// array($sport,'Golf');
-// var_dump($sport);
+// for($i=1;$i<5;$i++){
+//     for($j=1;$j<=$i;$j++){
+//         echo "*";
+//     }
+//     echo '<br>';
+// }
 
-// array_pop($sport); // e hek elemntin e fundit ne array
+$grade=array(
+    'math'=> '3' ,
+     'art'=>'5' , 
+     'history' => '4' , 
+     'music' => '4'
+);
+// echo "math grade is " . $grade ['math'];
 
-// array_unshift($sport,'Golf'); // e shton elemntin ne fillim te array
-
-//array_shift($sport); // e hek elemntin e par ne array
-
-// $output=array_slice($sport,3); // I merr elementet prej 3 dej nfund
-// $output1=array_slice($sport,-2,1); // i merr elementet prej -2 dhe prej tyre vetem nje element
-// $output2=array_slice($sport,0,3)// i merr prej element 0, 3 elementet e tjra ne vazhdin
-// var_dump($output2);
-
-
-$myvalue={12,45,63,3};
-$average=array_sum($value)/7;
-var_dump($average);
-
-$dogs=array(
-    array('Chihuahua','Mexico',20),
-    array('Husky','Siberia',15),
-    array('Bulldog','England',10);
-)
-
-echo $dogs[0][0] . "Origin: Mesico" .$dogs[0][1] ."life span:". $dogs[0][2]
+foreach ($grade as $subject=>$grade){
+    echo "subject: " . $subject . ", grade: " . $grade;
+    echo "<br>";
+}
 ?>
