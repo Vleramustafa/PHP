19c646ef86414889419b66c7e61df1aa05073892
drennii <kryeziudren6@gmail.com>
2024-11-08 16:50:19 +0100

    Add files via upload

---
+++ PHP/mysql.php
@@ -0,0 +1,16 @@
+<?php
+
+$host='localhost';
+$user='root';
+$pass='';
+
+try{
+    $conn=new PDO("mysql:host=$host" , $user ,$pass);
+
+    echo "Connect";
+
+    }catch(Exception $e){
+      echo "Not Connected";
+    }
+
+?>
