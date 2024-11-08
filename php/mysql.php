38ec173d3c5c1496835abec2837886abc60806c7
Artur-Halili <haliliartur894@gmail.com>
2024-11-08 16:50:14 +0100

    Add files via upload

---
+++ php/mysql.php
@@ -0,0 +1,19 @@
+<?php
+
+$host='localhost';
+$user='root';
+$pass='';
+
+try{
+
+  $conn=new PDO("mysql:host=$host" , $user , $pass);
+
+  echo "Connect";
+
+}catch(Exception $e){
+    echo "Not Connected";
+}
+
+
+
+?>
