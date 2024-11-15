a4c872a5342ed0fc2e7364a46a9a420469197b22
melisa-2 <melisaterstenaa@gmail.com>
2024-11-15 16:52:18 +0100

    Add files via upload

--- mysql/index.php
+++ mysql/index.php
@@ -1,26 +1,25 @@
 <?php
-    $host='localhost';
-    $db='testdbase';
-    $user='root';
-    $pass='';
+   $host='localhost';
+   $db='testdbase';
+   $user='root';
+   $pass='';
 
-    try{
-        $conn=new PDO("mysql:host=$host;dbname=$db",$user,$pass);
-        $sql="CREATE TABLE user(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(30) NOT NULL ,
-        pass VARCHAR(50) NOT NULL )";
+   try{
+    $conn=new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+    $sql="CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR (30) NOT NULL,
+    pass VARCHAR(50) NOT NULL)";
 
-        $conn->exec($sql);
-        echo "Table created successfully";
+    $conn->exec($sql);
+    echo "table created successfully";
 
-        // $sql="CREATE DATABASE testdbase";
+    //$sql="CREATE DATABASE testdbase";
 
-        // $conn->exec($sql);
+    //$conn->exec($sql);
 
-        // echo "Database is created";
-    }catch(Exception $e){
-        // echo "Database not created, somthing went wrong";
-        echo "Error creating table: " . $e->getMessage();
-    }
-
+    //echo "Database is created";
+   }catch(Exception $e){
+     //echo "Database not created,something went wrong";
+     echo "error creating table: " . $e->getMessage();
+   }
 ?>
