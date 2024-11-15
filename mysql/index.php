c1a373c9f68bd2015bef1ee7cdc6ab4260edb35f
erinasmajli <erinasmajli4@gmail.com>
2024-11-15 16:49:18 +0100

    Add files via upload

---
+++ mysql/index.php
@@ -0,0 +1,24 @@
+<?php
+    $host='localhost';
+    $db='testdbase';
+    $user='root';
+    $pass='';
+
+    try{
+        $conn=new PDO("mysql:host=$host;dbname=$db",$user,$pass);
+        $sql="CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(30) NOT NULL , 
+        pass VARCHAR(50) NOT NULL)";
+
+        $conn->exec($sql);
+        echo "Table created successfully";
+
+        // $sql="CREATE DATABASE testdbase";
+
+        // $conn->exec($sql);
+        // echo "Database is created";
+    }catch(Exception $e){
+        // echo "Database not created, something went wrong!";
+        echo "Error creating table: " . $e->getMessage();
+    }
+?>
