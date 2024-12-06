7400ff9ea955bb21f3c07f1c44861865bdc38567
erinasmajli <erinasmajli4@gmail.com>
2024-12-06 16:42:09 +0100

    Add files via upload

--- php/index.php
+++ php/index.php
@@ -1,32 +1,42 @@
- <?php
-
-// $username=$_GET['username'];
-// $password=$_GET['password'];
-// echo $username;
-// echo "<br>";
-// echo $password;
-
+<?php
+include_once('config.php');
+$sql='SELECT * FROM users';
+$getUsers=$connect->prepare($sql);
+$getUsers->execute();
+$users=$getUsers->fetchAll();
 ?>
 
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lesson 10</title>
 </head>
 <body>
-    <!-- <form>
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" placeholder="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" placeholder="Password"><br><br>
-        <input type="submit" value="submit">
-    </form> -->
-    <form action="add.php" method='POST'>
-        <input type="text" id="name" name="name" placeholder="name"><br>
-        <input type="text" id="surname" name="surname" placeholder="surname"><br>        
-        <input type="text" id="email" name="email" placeholder="email"><br>
-        <button type='submit' name='submit'>Add</button>
-    </body>
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Name</th>
+            <th>Email</th>
+        </thead>
+        <tbody>
+            <?php
+                foreach($users as $user){
+            ?>
+
+            <tr>
+                <td><?= $user['id']?></td>
+                <td><?= $user['username']?></td>
+                <td><?= $user['name']?></td>
+                <td><?= $user['email']?></td>
+            </tr>
+
+            <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</body>
 </html>
