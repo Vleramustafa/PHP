70a90c31d6714108bf2d98c5b1cd990da5646caa
Lea Bekteshi <leabekteshi1@gmail.com>
2025-01-17 16:48:31 +0100

    lesson12

---
+++ php/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>
+        
+        table
+        {
+            border: 1px solid black;
+        }
+
+
+        tr,td,th
+        {
+            border: 1px solid black;
+            
+        }
+        table,tr,td
+        {
+            border-collapse: collapse;
+        }
+        td
+        {
+            padding: 10px;
+        }
+
+
+    </style>
+</head>
+<body>
+
+<?php
+
+?>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Username</th>
+      <th scope="col">Name</th>
+      <th scope="col">Surname</th>
+      <th scope="col">Email</th>
+      <th scope="col">Update</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td><?= $users['id'] ?></td>
+      <td><?= $users['username']?></td>
+      <td><?= $users['name']?></td>
+      <td><?= $users['surname']?></td>
+      <td><?= $users['email']?></td>
+      <a href='delete.php?id=$user['id']>DELETE</a>
+    </tr>
+  
+  </tbody>
+</table>
+
+
+     <a href="index.php">Add User</a>
+    
+</body>
+</html>
