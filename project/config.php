099fb8f906fbb7514a62b0a147469bb5e76ef475
Artur Halili <haliliartur894@gmail.com>
2025-01-24 16:41:16 +0100

    message

---
+++ project/config.php
@@ -0,0 +1,12 @@
+<?php
+    $server='localhost';
+    $username='root';
+    $password='';
+    $dbname='project';
+    
+    try{
+        $connect=new PDO("mysql:host=$server;dbname=$dbname",$username,$password);
+    }catch(Exception $e){
+        echo "Something went wrong";
+
+    }
