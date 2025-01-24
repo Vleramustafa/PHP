93aca0557a4b306b939c6fe5e272ca4f76b8d33f
Fion-bit <fionbora@gmail.com>
2025-01-24 16:41:17 +0100

    challange

---
+++ project/dashboard.php
@@ -0,0 +1,83 @@
+<?php
+
+include_once('config.php');
+if(empty($_SESSION['username'])){
+    header('Location:login.php');
+};
+$sql=" SELECT * FROM users";
+$getUsers=$connect->prepare($sql);
+$getUsers->execute();
+$users_data=$getUsers->fetchAll();
+
+include_once('header.php');
+
+
+?>
+
+</style>
+
+
+
+          </style>
+
+
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Welcome, <i> <?php echo $_SESSION['username']; ?> </i></a>
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="logout.php">Sign out</a>
+    </li>
+  </ul>
+</nav>
+
+
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="dashboard.php">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="productDashboard.php">
+              <span data-feather="home"></span>
+               Product Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <?php foreach ($users_data as $user_data){ ?>
+
+                <a class="nav-link" href="profile.php>id<?=$user_data['id'];?>">
+
+                <?php} ?>
+
+            
+
+          <a href="addProduct.php" id="product"> Add Product</a>
+
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Surname</th>
+              <th>Email</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+
+          </tbody>
+        </table>
+      </div>
+    </main>
+  </div>
+</div>
+
+
+
