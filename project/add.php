7641a9e7127959ca2d6feb7ec6ba260c5f7827fa
Erina Smajli <erinasmajli4@gmail.com>
2025-01-24 16:40:57 +0100

    project

---
+++ project/add.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add a user</title>
+</head>
+<body>
+<a href="index.php">Dashboard</a>
+    <form action="add.php" method="POST">
+        <input type="text" name="name" placeholder="Name"></br>
+        <input type="text" name="surname" placeholder="Surname"></br>
+        <input type="email" name="email" placeholder="Email"></br>
+        <button type="submit" name="submit">Add</button>
+    </form>
+</body>
+</html>
