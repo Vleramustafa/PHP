7e343804f5a8ed63ed4698e121dfd7635ae1d70d
Elona Ramiqi <ramiqielona@gmail.com>
2025-02-07 16:51:04 +0100

    ushtrime

---
+++ mms/loginLogic.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+include_once('config.php');
+
+if(isset($_POST['submit'])){
+
+    $username=$_POST['username'];
+
+    $password=$_POST['password'];
+
+    if(empty($username) || empty($password)){
+        echo "Please fill all the fields";
+    }else{
+        $sql="SELECT id,name,username,surname,email,password,is_admin FROM users WHERE username=:username";
+
+        $selectUsers=$conn->prepare($sql);
+        $selectUser->bindParam(":username",$username);
+        $selectUser->execute();
+        $data=$selectUser->fetch();
+
+        if($data==false){
+            echo "The user does not exist";
+        }else{
+            if(password_verify($password,$data['password'])){
+                $_SESSION['id']=$data['id'];
+                $_SESSION['username']=$data['username'];
+                $_SESSION['name']=$data['name'];
+                $_SESSION['surname']=$data['surname']; 
+                $_SESSION['emaail']=$data['email'];
+                $_SESSION['is_admin']=$data['is_admin'];
+                
+                header('Location:dashboard.php');
+            }else{
+                echo "The password is not valid";
+            }
+        }
+    }
+}
