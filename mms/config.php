44851890860462133bccd9ad3e8daccf054e4f83
Erina Smajli <erinasmajli4@gmail.com>
2025-02-07 16:50:54 +0100

    projekt mms

--- mms/config.php
+++ mms/config.php
@@ -2,10 +2,10 @@
 $user='root';
 $pass='';
 $server='localhost';
-$dbname='mms1';
+$dbname='mms';
 
 try{
     $conn=new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
 }catch(PDOException $e){
     echo "error: " . $e->getMessage();
-}
+} 
