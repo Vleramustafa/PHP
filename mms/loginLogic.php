4d58041fbe3fa4bdebbef0e025cbbfb28e8f681c
Fion Mustafa <fionbora@gmail.com>
2025-02-07 16:51:17 +0100

    projekti

---
+++ mms/loginLogic.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+include_once('config.php');
+
+if(isset($_POST['submit'])){
+
+    $username=$_POST['username'];
+
+    $password=$_POST['password'];
+
+    if(empty($username) ||($password)){
+        echo "Please fill all the field";
+    }else{
+        $sql="SELECT id,name,username.surname,email,password,is_admin FROM users WHERE username=:username";
+
+        $selectUser=$conn->prepare($sql);
+        $selectUser->bindParam(":usernaem",$username);
+        $selectUser->execute();
+        $data= $selectUser->fetch();
+
+        if($data==false){
+            echo "The user does not exist";
+        }else{
+            if(password_verify($password,$data['password'])){
+                $_SESSION['id']=$data['id'];
+                $_SESSION['username']=$data['username'];
+                $_SESSION['name']=$data['name'];
+                $_SESSION['surname']=$data['surname'];
+                $_SESSION['email']=$data['email'];
+                $_SESSION['is_admin']=$data['is_admin'];
+
+                header('Location:dashboard.php');
+
+            }else{
+                echo "The password is not valid"
+            }
+        }
+    }
+}
