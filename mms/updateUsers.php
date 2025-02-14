9e91db504f0744efb7b5ff57c1913a269e8fd15c
Lea Bekteshi <leabekteshi1@gmail.com>
2025-02-14 16:50:10 +0100

    leaaaa

---
+++ mms/updateUsers.php
@@ -0,0 +1,11 @@
+<?php
+include_once('config.php');
+
+if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $name=$_POST['name'];
+    $username=$_POST['username'];
+    $email=$_POST['email'];
+
+    $sql="UPDATE"
+}
