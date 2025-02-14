935ee53e9d33ec08ad9ef433e647229229f71a03
Elona Ramiqi <ramiqielona@gmail.com>
2025-02-14 16:51:13 +0100

    ushtrime

---
+++ mms/edit.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+include_once('config.php');
+
+
+
+
+
+$id=$_GET['id'];
+
+$sql="SELECT * FROM users WHERE id=:id";
+
+$selectUser=$conn->prepare($sql);
+$selectUser->bindParam(':id',$id);
+$selectUser->execute();
+
+$user_data=$selectUser->fetch();
+?>
+
