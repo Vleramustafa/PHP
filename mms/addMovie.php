de51c7dd99f8ca3e0ade33e7df8435273d1e9ffc
Atik Mexhuani <atikmexhunai69@gmail.com>
2025-02-21 16:48:46 +0100

    poasd

---
+++ mms/addMovie.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('config.php');
+
+if(isset($_POST['submit'])){
+
+    $movie_name=$_POST['movie_name'];
+    $movie_desc=$_POST['movie_desc'];
+    $movie_quality=$_POST['movie_quality'];
+    $movie_rating=$_POST['movie_rating'];
+    $movie_image=$_POST['movie_image'];
+
+    $sql="INSERT INTO movie(movie_name,movie_desc,movie_quality,movie_image,movie_rating) VALUES (movie_name,movie_desc,movie_quality,movie_image,movie_rating)";
+
+    $insertMovie=$conn->prepare($sql);
+    $insertMovie->bindParam(':movie_name',$movie_name);
+    $insertMovie->bindParam(':movie_desc',$movie_desc);
+    $insertMovie->bindParam(':movie_quality',$movie_quality);
+    $insertMovie->bindParam(':movie_image',$movie_image);
+    $insertMovie->bindParam(':movie_rating',$movie_rating);
+
+    $insertMovie->execute();
+
+    header('Location:movie.php');
+}
+    
+    
+
+    
