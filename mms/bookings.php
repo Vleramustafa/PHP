149310b9f6a15afdffe2cb8b05636c5ffa6f037a
sde38695 <sde38695@gmail.com>
2025-02-28 16:43:26 +0100

    z
    zzzzz

---
+++ mms/bookings.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+include_once('config.php');
+
+$user_id=$_SESSION['id'];
+
+if($_SESSION['is_admin']=='true'){
+    $sql="SELECT movies.movie_name,users.email,bookings.id,bookings.nr_tickets,bookings.date,bookings.is_approved,bookings.time FROM movies
+    INNER JOIN bookings ON movies.id=bookings.movie_id
+    INNER JOIN users ON users.id=bookings.user_id"
+
+    $selectBookings=$conn->prepare($sql);
+    $selectBookings->execute();
+
+    $bookings_data=$selectBookings->fetchAll();
+}else{
+$sql="SELECT movies.movie_name,users.email,bookings.id,bookings.nr_tickets,bookings.date,bookings.is_approved,bookings.time FROM movies
+INNER JOIN bookings ON movies.id=bookings.movie_id
+INNER JOIN users ON users.id=bookings.user_id WHERE bookings,user_id=:user_id";
+$selectBookings=$conn->prepare($sql);
+
+$selectBookings->bindParam(':user_id',$user_id);
+$selectBookings->execute();
+$bookings_data=$selectBookings->fetchAll();
+}
