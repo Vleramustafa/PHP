257370f2413ba15a541cbe163f8e3d22839709fe
Melisa Terstena <melisaterstenaa@gmaail.com>
2025-03-21 15:38:27 +0100

    m

--- personalproject/mms.sql
+++ personalproject/mms.sql
@@ -19,23 +19,36 @@ CREATE TABLE users(
      product_image varchar(255) NOT NULL
     )
 
--- TABLE STRUCTURE FOR TABLE 'BOOKINGS'
-       CREATE TABLE bookings(
+-- TABLE STRUCTURE FOR TABLE 'ORDERS'
+       CREATE TABLE orders(
      id int(11) NOT NULL ,
      user_id int(11) NOT NULL,
      product_id int(11) NOT NULL,
-     nr_tickets int(255) NOT NULL,
-     date varchar(255) NOT NULL,
      is_approved varchar(255) NOT NULL,
-     time varchar(255) NOT NULL
     )
 
  ALTER TABLE users ADD PRIMARY KEY (id);
   ALTER TABLE productS ADD PRIMARY KEY (id);
-   ALTER TABLE bookings ADD PRIMARY KEY (id);
+   ALTER TABLE orders ADD PRIMARY KEY (id);
 
 
 
 ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE products MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE bookings MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE orders MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+
+INSERT INTO `users` 
+(`id`, `name`, `surname`, `username`, `email`, `password`,`confirm_password`, `is_admin`)
+ VALUES (1, 'Melisa', 'Terstena', 'melisa2', 'melisa2@mail.com', '12345678','12345678','1'), 
+ (2, 'Elona', 'Ramiqi', 'elonaramiqi', 'elonaramiqi@mail.com', '12345678','12345678','0');
+
+
+ INSERT INTO `products` (`id`, `product_name`, `product_quality`, `product_rating`, `product_image`) VALUES
+(1,  '3D', 10, 'zgjoi.jpg'),
+(2,  '2D', 7, 'fastandfurious.jpg'),
+(3,  '6D', 7, 'venom.png');
+
+
+INSERT INTO `orders` (`id`, `user_id`, `product_id`,`is_approved`) VALUES
+(3, 2, 1, 'true');
