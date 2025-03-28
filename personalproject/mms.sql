94c90d56d75f2b942aeca5469e8e891f9bd264b7
Elona Ramiqi <ramiqielona@gmail.com>
2025-03-28 16:09:59 +0100

    pp

--- personalproject/mms.sql
+++ personalproject/mms.sql
@@ -18,8 +18,25 @@ CREATE TABLE clothes (
     id int(11) NOT NULL,
     user_id int(255) NOT NULL,
     clothes_id int(11) NOT NULL
-    
-
-
-
  )
+
+ALTER TABLE users ADD PRIMARY KEY (id)
+
+ALTER TABLE clothes ADD PRIMARY KEY (id);
+ALTER TABLE orders ADD PRIMARY KEY (id);
+
+ALTER TABLE users MODIFY id int(11) NOT NULL AUTO_INCREMENT
+ALTER TABLE clothes MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE orders MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `clothes` (`id`, `clothes_image`, `clothes_rating`, `clothes_price`, `clothes_quantity`) VALUES 
+(1, 'e.jpg', 10, '30$'),
+(2, 'photo(1).webp', 7, '15$'),
+(3, 'el.jpg', 9, '25$');
+
+INSERT INTO `orders` (`id`, `user_id`, `clothes_id`, `is_approved`) VALUES
+(3,2,1, 'true');
+
+
+
+ 
