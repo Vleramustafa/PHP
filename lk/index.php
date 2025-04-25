3eee5aa31466114c14e2091a7c13258aa7f54254
Leaa46 <leabekteshi1@gmail.com>
2025-04-25 06:12:05 -0700

    Add files via upload

---
+++ lk/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="We have a wide collection of electronics, phones, books, and games">
+    <meta name="keywords" content="phones, books, games, electronics">
+<link rel="stylesheet" href="css/styles.css">
+
+    
+    <title>Our Store</title>
+</head>
+<body>
+
+<nav>
+
+<div class="brand">Our store</div>
+<div class="links">
+    <a data-active="index" href="index.php">Home</a>
+    <a data-active="about" href="about.php">About</a>
+    <a data-active="contact" href="contact.php">Contact</a>
+</div>
+</nav>
+
+<header></header>
+
+
+<main></main>
+
+
+<footer></footer>
+    
+
+
+
+
+
+
+<script src="javascript/script.js"></script>
+</body>
+</html>
